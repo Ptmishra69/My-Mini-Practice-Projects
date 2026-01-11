94e1c47377915310dc64eb5ebed6e76c9a10abeb
Ptmishra69 <blindshortgamer@gmail.com>
2026-01-11 16:06:11 +0530

    practicing

---
+++ JS_Practice/Practice_1_text_changer/index.js
@@ -0,0 +1,5 @@
+content = document.getElementById("para")
+button = document.getElementById("button")
+button.addEventListener('click',function() {
+    content.textContent = "Saregama Padha Nisa Sanidhapa Magaresa"
+})
